5e2d3cd18f17cb7985d134a291e588ee5a09ed1f
Mahaveer86619 <psatyam86619@gmail.com>
2023-05-01 00:10:47 +0530

    volley used to get meme api glide used to load meme into image view meme loading and next btn's functionality added

--- app/src/main/java/com/example/memeapp/MainActivity.java
+++ app/src/main/java/com/example/memeapp/MainActivity.java
@@ -2,13 +2,85 @@ package com.example.memeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.request.RequestListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+@GlideModule
 public class MainActivity extends AppCompatActivity {
+
+    ImageView meme_image;
+    Button share,next;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        meme_image = findViewById(R.id.meme_image);
+        share = findViewById(R.id.btn_share);
+        next = findViewById(R.id.btn_next);
+
+
+        loadmeme();
+
+
+    }
+
+    private void loadmeme() {
+
+// Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://meme-api.com/gimme";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String meme = response.getString("url");
+                            Glide.with(MainActivity.this).load(meme).into(meme_image);
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle error
+
+                    }
+                });
+
+// Access the RequestQueue through your singleton class.
+        queue.add(jsonObjectRequest);
+    }
+    public void sharememe(View view) {
+    }
+
+    public void nextmeme(View view) {
+        loadmeme();
     }
 }
