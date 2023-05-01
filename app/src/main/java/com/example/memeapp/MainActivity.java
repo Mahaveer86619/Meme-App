ae60720da7d0a0e3ef8444b4f0cc6ef2d644c3cf
Mahaveer86619 <psatyam86619@gmail.com>
2023-05-01 12:05:28 +0530

    Finished App

--- app/src/main/java/com/example/memeapp/MainActivity.java
+++ app/src/main/java/com/example/memeapp/MainActivity.java
@@ -2,35 +2,26 @@ package com.example.memeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.drawable.Drawable;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.request.RequestListener;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 
 @GlideModule
 public class MainActivity extends AppCompatActivity {
 
     ImageView meme_image;
     Button share,next;
-    ProgressBar progressBar;
+    String currentImageUrl = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,46 +32,44 @@ public class MainActivity extends AppCompatActivity {
         share = findViewById(R.id.btn_share);
         next = findViewById(R.id.btn_next);
 
-
-        loadmeme();
-
+        load_meme();
 
     }
 
-    private void loadmeme() {
+    private void load_meme() {
 
 // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
+//        RequestQueue queue = Volley.newRequestQueue(this);
         String url = "https://meme-api.com/gimme";
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            String meme = response.getString("url");
-                            Glide.with(MainActivity.this).load(meme).into(meme_image);
-                        } catch (JSONException e) {
-                            throw new RuntimeException(e);
-                        }
+                (Request.Method.GET, url, null, response -> {
+                    try {
+                        //select what you want from the response after hitting the api, here only meme url was needed
+                        currentImageUrl = response.getString("url");
+                        Glide.with(MainActivity.this).load(currentImageUrl)
+                                .fitCenter()
+                                .into(meme_image);
+                    } catch (JSONException e) {
+                        Toast.makeText(MainActivity.this, "Error! check your internet connection", Toast.LENGTH_SHORT).show();
+                        throw new RuntimeException(e);
                     }
-                }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // Handle error
-
-                    }
+                }, error -> {
+                    // Handle error
                 });
 
 // Access the RequestQueue through your singleton class.
-        queue.add(jsonObjectRequest);
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
     }
-    public void sharememe(View view) {
+    public void share_meme(View view) {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("text/plain");
+        i.putExtra(Intent.EXTRA_TEXT, "Hey! checkout this cool meme I got from Meme App " + currentImageUrl);
+        Intent chooser = Intent.createChooser(i, "Share this meme using...");
+        startActivity(chooser);
     }
 
-    public void nextmeme(View view) {
-        loadmeme();
+    public void next_meme(View view) {
+        load_meme();
     }
 }
